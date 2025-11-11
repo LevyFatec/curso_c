6b96617b8050090dff40a7f4795f5c7c0cf3e5c3
LevyFatec <143909268+LevyFatec@users.noreply.github.com>
2025-11-10 22:13:36 -0300

    2.0.1 fix bug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-
-const SUPABASE_URL = process.env.SUPABASE_URL || 'https://hlrewmxnzldqzpahmzgt.supabase.co';
-const SUPABASE_KEY = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscmV3bXhuemxkcXpwYWhtemd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzODQwMjMsImV4cCI6MjA3Nzk2MDAyM30.CxSkMw-j9SiCSkclQF0xT_E_jPtycWMw3otPEVyY5q0';
-
+const SUPABASE_URL = 'https://hlrewmxnzldqzpahmzgt.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhscmV3bXhuemxkcXpwYWhtemd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzODQwMjMsImV4cCI6MjA3Nzk2MDAyM30.CxSkMw-j9SiCSkclQF0xT_E_jPtycWMw3otPEVyY5q0';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
